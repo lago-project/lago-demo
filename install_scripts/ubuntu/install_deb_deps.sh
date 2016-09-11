04047d713b62fe9abe8a0516a26fe79bca10f49c
gbenhaim <galbh2@gmail.com>
2016-09-11 20:57:59 +0300

    Added an installation manual for Ubuntu
    1. Added Lago installation manual for ubuntu.
    2. Added list of python libs required by lago that
       can be found on ubuntu repos.
    3. Changed the installation script for .deb packages
       to be non-interactive.
    
    Signed-off-by: gbenhaim <galbh2@gmail.com>

--- install_scripts/ubuntu/install_deb_deps.sh
+++ install_scripts/ubuntu/install_deb_deps.sh
@@ -2,16 +2,16 @@
 
 # core components
 
-sudo apt-get install qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils
+sudo apt-get install -y qemu-kvm libvirt-bin ubuntu-vm-builder bridge-utils
 
 # python-magic version on PyPi is not suited for lago
 
-sudo apt-get install python-magic
+sudo apt-get install -y python-magic
 
 # deps for python cryptography
 
-sudo apt-get install build-essential libssl-dev libffi-dev python-dev
+sudo apt-get install -y build-essential libssl-dev libffi-dev python-dev
 
 # for guestfs
 
-sudo apt-get install libguestfs-tools python-guestfs
+sudo apt-get install -y libguestfs-tools python-guestfs
