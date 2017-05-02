9d50dd2668a0b3cd0435a3962e29e108e28fdd67
Nadav Goldin <ngoldin@redhat.com>
2017-05-02 21:23:59 +0300

    install_lago: allow only configuring permissions for a user
    Add the '-p' option which will only configure the needed permissions for
    the user. This must be called after Lago is installed.
    
    Example:
    
    sudo ./install_lago.sh --user user1
    
    ... some time afterwards ...
    
    sudo ./install_lago.sh --user user2 -p
    
    Will allow 'user2' to run Lago as well.
    
    Signed-off-by: Nadav Goldin <ngoldin@redhat.com>

--- tests/003-start-vm/run.sh
+++ tests/003-start-vm/run.sh
@@ -2,6 +2,7 @@
 
 readonly RUN_DIR="$(dirname "${BASH_SOURCE[0]}")"
 readonly USERNAME="dummy_user"
+readonly SECONDARY_USER="dummy_user2"
 readonly INIT_FILE="$RUN_DIR/init-nested"
 
 function start_vm() {
@@ -13,13 +14,15 @@ export LIBGUESTFS_BACKEND=direct
 export LIBGUESTFS_DEBUG=1 LIBGUESTFS_TRACE=1
 lago init $INIT_FILE
 lago start
-lago shell nested -c 'hostname'
+lago shell nested -c 'hostname' && lago destroy --yes
+
 EOS
 EOF
 }
 
 function main() {
     start_vm "$USERNAME"
+    start_vm "$SECONDARY_USER"
 }
 
 main "$@"
